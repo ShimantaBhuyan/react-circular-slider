8d49859299fb089eef1433f4c221f43c5a73b7f0
Shimanta Bhuyan <bhuyanshimanta@gmail.com>
2020-05-26 23:36:38 +0530

    Updates

--- precache-manifest.ec1f5ace8cba90b6cbf7c3affe919eba.js
+++ precache-manifest.ec1f5ace8cba90b6cbf7c3affe919eba.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7db2dd8c5c057410373c1cd079657dd0",
+    "revision": "9c6a5523bd96f3053ab6a32a22939b62",
     "url": "/react-circular-slider/index.html"
   },
   {
-    "revision": "6b76c5efd6b12f217a85",
-    "url": "/react-circular-slider/static/css/main.01192246.chunk.css"
+    "revision": "694abde5b6411e7f43f2",
+    "url": "/react-circular-slider/static/css/main.7c6fb68e.chunk.css"
   },
   {
     "revision": "8354e88208d47b25f722",
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-circular-slider/static/js/2.9187ac00.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6b76c5efd6b12f217a85",
-    "url": "/react-circular-slider/static/js/main.74540e8b.chunk.js"
+    "revision": "694abde5b6411e7f43f2",
+    "url": "/react-circular-slider/static/js/main.1c3263f7.chunk.js"
   },
   {
     "revision": "415bcfe151977a428707",
     "url": "/react-circular-slider/static/js/runtime-main.44242f48.js"
   },
   {
-    "revision": "8197afbc5e3d1a898d444624455ffaa1",
-    "url": "/react-circular-slider/static/media/drag.8197afbc.svg"
+    "revision": "3f95288c088f34ecd83ecf3dd5e1a977",
+    "url": "/react-circular-slider/static/media/drag.3f95288c.svg"
   },
   {
     "revision": "8c505251b42d39bf4f1a438d93c3d6f1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-circular-slider/precache-manifest.ebf48bb53db03315eef15b666b86e7bc.js"
+  "/react-circular-slider/precache-manifest.ec1f5ace8cba90b6cbf7c3affe919eba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-circular-slider/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
